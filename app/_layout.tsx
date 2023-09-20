38595787a1a6024ca2b899ba0d32fc05bbb030b2
Orinameh <memefacto@yahoo.com>
2023-09-20 11:23:11 +0100

    regroup public routes into public folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,14 +23,14 @@ export default function GasaltLayout() {
   return (
     <Stack>
       {/* Public routes */}
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="sendCode" options={{ headerShown: false }} />
-      <Stack.Screen name="loginCode" options={{ headerShown: false }} />
-      <Stack.Screen name="setDomain" options={{ headerShown: false }} />
-      <Stack.Screen name="domainSet" options={{ headerShown: false }} />
-      <Stack.Screen name="transactionPin" options={{ headerShown: false }} />
-      <Stack.Screen name="congratulations" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/onboarding" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/login" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/sendCode" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/loginCode" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/setDomain" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/domainSet" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/transactionPin" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)/congratulations" options={{ headerShown: false }} />
 
      {/* Authenticated routes */}
      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,23 @@
-import { DefaultView } from "@/components/Defaults"
 import { useGlobalState } from "@/sdk/state";
-import { Redirect } from "expo-router";
+import { Redirect, Slot } from "expo-router";
 
 // Entry point of the app. The session determines what should show
 function Index() {
     const { session } = useGlobalState();
 
     if (session.loggedInBefore) {
-        return <Redirect href={"/login"} />;
+        return <Redirect href={"/(public)/login"} />;
     }
 
     if (session.isFirstTimeUser) {
-        return <Redirect href={"/onboarding"} />;
+        return <Redirect href={"/(public)/onboarding"} />;
     }
 
     // Redirecting to tabs causes infinite error. Need to find a solution for this.
     if (session.currentlyLoggedIn) {
         return <Redirect href={"/(drawer)/(tabs)/wallet"} />;
     }
-    return <DefaultView></DefaultView>
-
+    return <Slot />
 }
 
 export default Index
