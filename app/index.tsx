e3ad3682fd98762ae51d362ecb76e6c20b4f7813
Orinameh <memefacto@yahoo.com>
2023-09-20 11:23:11 +0100

    regroup public routes into public folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,23 @@
-import { DefaultView } from "@/components/Defaults"
 import { useGlobalState } from "@/sdk/state";
-import { Redirect } from "expo-router";
+import { Redirect, Slot } from "expo-router";
 
 // Entry point of the app. The session determines what should show
 function Index() {
     const { session } = useGlobalState();
 
-  if(session.loggedInBefore) {
-    return <Redirect href={"/(public)/login"} />;
-  }
+    if (session.loggedInBefore) {
+        return <Redirect href={"/(public)/login"} />;
+    }
 
-  if(session.isFirstTimeUser) {
-    return <Redirect href={"/(public)/onboarding"} />;
-  }
+    if (session.isFirstTimeUser) {
+        return <Redirect href={"/(public)/onboarding"} />;
+    }
 
+    // Redirecting to tabs causes infinite error. Need to find a solution for this.
+    if (session.currentlyLoggedIn) {
+        return <Redirect href={"/(drawer)/(tabs)/wallet"} />;
+    }
+    return <Slot />
 }
 
 export default Index
